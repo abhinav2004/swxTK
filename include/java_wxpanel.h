51b6428049cecccf74ca11fef9a466e8a52d9dee
abhinav2004 <ahardyx@gmail.com>
2014-03-17 13:20:50 +0530

    Cleared swxTK. Moved everything to swxTK-java and swxTK-scala.

--- include/java_wxpanel.h
+++
@@ -1,8 +0,0 @@
-#include "java_wxframe.h"
-
-class java_wxpanel : public wxPanel {
-public:
-	java_wxpanel(java_wxframe* parent, int id, int x, int y, int width, int height, long style, std::string name);
-	
-};
-
